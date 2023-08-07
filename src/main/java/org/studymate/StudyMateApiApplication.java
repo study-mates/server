796cffec869d885026768a93612e3cf331507651
fenrir <salzte0506@gmail.com>
2023-08-07 12:36:54 +0900

    feat:initial commit

---
+++ src/main/java/org/studymate/StudyMateApiApplication.java
@@ -0,0 +1,13 @@
+package org.studymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMateApiApplication.class, args);
+	}
+
+}
